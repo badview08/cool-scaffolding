3b8abd23c5b600c6662f02523565f24056807bde
badview08 <jonesgabriel21@gmail.com>
2015-06-03 11:50:48 -0400

    added scss files to project

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -3,5 +3,5 @@ this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
     return "";
 },"useData":true});
 this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<form class=\"login-form\">\n      <input type=\"text\" placeholder=\"Username\">\n      <input type=\"submit\" value=\"login\">\n </form>";
+    return "<form class=\"login-form-username\">\n      <input type=\"text\" placeholder=\"Username\">\n      <input type=\"submit\" value=\"login\">\n </form>";
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,26 @@
 (function(){
   'use strict';
-
+  	var username ='';
+    
   $(document).ready(function(){
+    
+    route();
     
     $('.login-form').on('submit', 'login-form', function(event){
       event.preventDefault();
+      console.log($(this).find('.login-user-name'));
       window.location.hash = '/chat';
     });
     
-    $(window).on('hashchange'), function(event){}
+    $(window).on('hashchange'), function(event){
       event.preventDefault();
       console.log(window.location.hash);
+    };
+    $(window).on('hashchange', function(event){
+      event.preventDefault();
+      route();
     });
+  });
     
     function route(){
       switch (window.location.hash){
@@ -21,6 +30,10 @@
       case '#/chat':
         $('.application').htmls(JST['chat']());
         break;
-    }
-  };
+    };
+    function renderChat() {
+      $('.application').html(JST['chat']());
+      console.log('username: ', username);
+    };
+    };
 })();
