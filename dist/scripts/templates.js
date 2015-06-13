595f2da4772538cbe9123ac1a310bd46ba1b12df
badview08 <jonesgabriel21@gmail.com>
2015-06-13 13:25:53 -0400

    redone 5.3 assignemt for ajax program

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -4,8 +4,25 @@ this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
 
   return "<div class=\"total-container\">\n	<div class=\"container-sidebar\">\n		<img class=\"profile-pic\" src=\"/profile/usr.jpg\"></img>\n		<ul class=\"username\">"
     + this.escapeExpression(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"username","hash":{},"data":data}) : helper)))
-    + "</ul>\n	</div>\n	<div class=\"main-container\">\n		<div class=\"message-entry-container\">\n			<h1 class=\"textlocator\">text will go here.</h1>\n				<div class=\"TextEntry\">\n					<entry class=\"text-entry-field\"></entry>\n			<form class=\"createmessage\">\n  			<input class=\"typemessage\" type=\"text\" placeholder=\"Type your message here\">\n  			<input class=\"submit-message\" type=\"submit\" value=\"Send\">\n			</form>\n				</div>\n		</div>\n	</div>\n\n</div>";
+    + "</ul>\n	</div>\n	<div class=\"main-container\">\n		<div class=\"message-entry-container\">\n			<ul class=\"chatlog\"> \n				\n			</ul>\n			<div class=\"messagelog\">\n				<entry class=\"text-entry-field\"></entry>\n			<form class=\"createmessage\">\n  			<input class=\"typemessage\" type=\"text\" placeholder=\"Type your message here\">\n  			<input class=\"submit-message\" type=\"submit\" value=\"Send\">\n			</form>\n				</div>\n		</div>\n	</div>\n\n</div>";
 },"useData":true});
 this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     return "<div class=\"login-container\">\n   <h1 class=\"loginProgam\">Chattery</h1>\n      <hr></hr>\n      <form class=\"login-form\">\n      <input type=\"text\" class=\"login-form-username\" placeholder=\"Username\">\n      <input type=\"submit\" value=\"login\">\n </form>   \n</div>\n\n ";
+},"useData":true});
+this["JST"]["message"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+
+  return "<li>Username: "
+    + alias3(((helper = (helper = helpers.username || (depth0 != null ? depth0.username : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"username","hash":{},"data":data}) : helper)))
+    + "</li>\n<li>Content: "
+    + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
+    + "</li>\n<li>Time: "
+    + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
+    + "</li>\n<li>Message ID: "
+    + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
+    + "\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,depth0,{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,106 @@
+//(function(){
+//  'use strict';
+//  	var username = localStorage.getItem('username');
+//    var message = [
+//     {
+//       username: '.login-form-username',
+//       created_at: new Date(),
+//       content:Math.random()
+//     }
+//   ]
+//    
+//  $(document).ready(function(){
+//    
+//    route();
+//    
+//    $(document).on('submit', '.login-form', function(event){
+//      event.preventDefault();
+//      username = ($(this).find('.login-form-username').val());
+//      localStorage.setItem('username', username);
+//      window.location.hash = '/chat';
+//    });
+//    
+//    $(window).on('hashchange', function(event){
+//      event.preventDefault();
+//      route();
+//    });
+//    $(window).on('hashchange', function(event){
+//      event.preventDefault();
+//      route();
+//    });
+//  });
+//    
+//    function route(){
+//      switch (window.location.hash){
+//      case '':
+//        $('.application').html(JST['login']());
+//        break;
+//      case '#/chat':
+//        renderChat()
+//        //$('.application').htmls(JST['chat']());
+//        break;
+//    };
+//    }
+//    function renderChat(){
+//      $('.application').html(JST['chat']());
+//      console.log(username);
+//   };
+//   
+//   function message(message) {
+//      this.username = username;
+//      this.created_at = Date;
+//      this.content = message;
+//      
+//    }
+//   $.ajax({
+//     url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/",
+//     type:"POST",
+//     data: {
+//       username: '.login-form-username',
+//       created_at: new Date(),
+//       content: "Hello"
+//     }
+//   })
+//   function deleteInvalidMessages(){
+//  $.ajax({
+//    url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/"
+//  }).then(function(messages) {
+//    console.log(messages);
+//    var invalid = _.reject(messages, function(message) {
+//      return message.hasOwnProperty('username')
+//          && message.hasOwnProperty('created_at')
+//          && message.hasOwnProperty('content')
+//    });
+//    console.log(invalid);
+//    invalid.forEach(function(message) {
+//      $.ajax({
+//        url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/" + message._id,
+//        type: "DELETE"
+//      })
+//    });
+//  });
+//} 
+//
+//})();
+
 (function(){
   'use strict';
-  	var username ='';
-    
-  $(document).ready(function(){
-    
-    route();
-    
-    $(document).on('submit', '.login-form', function(event){
-      event.preventDefault();
-      username = ($(this).find('.login-form-username').val());
-      window.location.hash = '/chat';
-    });
-    
-    $(window).on('hashchange', function(event){
-      event.preventDefault();
-      route();
-    });
-    $(window).on('hashchange', function(event){
-      event.preventDefault();
-      route();
-    });
+  
+  var username ='';
+  var message ='';
+  
+  $(document).on('submit', '.login-form', function(event){
+    event.preventDefault();
+    username = $(this).find('login-form-username').val();
+    window.chatRouter.navigate('/chat', {trigger: true});
   });
-    
-    function route(){
-      switch (window.location.hash){
-      case '':
-        $('.application').html(JST['login']());
-        break;
-      case '#/chat':
-        renderChat()
-        //$('.application').htmls(JST['chat']());
-        break;
-    };
-    }
-    function renderChat(){
-      $('.application').html(JST['chat']());
-      console.log('username: ', username);
-   };
-   var messages = [
-     {
-       username:"Jake",
-       created_at: new Date(),
-       content:Math.random()
-     }
-   ]
-   $.ajax({
-     url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/",
-     type:"POST",
-     data: {
-       username: 'Gabe',
-       created_at: new Date(),
-       content: "Hello"
-     }
-   })
-   function deleteInvalidMessages(){
-  $.ajax({
-    url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/"
-  }).then(function(messages) {
-    console.log(messages);
-    var invalid = _.reject(messages, function(message) {
-      return message.hasOwnProperty('username')
-          && message.hasOwnProperty('created_at')
-          && message.hasOwnProperty('content')
-    });
-    console.log(invalid);
-    invalid.forEach(function(message) {
-      $.ajax({
-        url: "http://tiny-lasagna-server.herokuapp.com/collections/messages/" + message._id,
-        type: "DELETE"
-      })
-    });
+  $(document).on('submit', '.new-message', function(event){
+    event.preventDefault();
+    message = $(this).find('.message-input').val();
+    postMessage();
   });
-} 
-})();
+  
+  var ChatRouter = Backbone.Router.extend({
+    
+  })
+});
